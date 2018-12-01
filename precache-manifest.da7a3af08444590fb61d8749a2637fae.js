d6c0c5d765bb49eeca00adcd619c7318ce7c3636
sidinsomniac <chatterjeesid1993@gmail.com>
2018-12-01 20:52:19 +0530

    Updates

---
+++ precache-manifest.da7a3af08444590fb61d8749a2637fae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "555091e63d5aa4204a43",
+    "url": "/Neigbourhood_Map/static/js/runtime~main.555091e6.js"
+  },
+  {
+    "revision": "a2351cc689155f902198",
+    "url": "/Neigbourhood_Map/static/js/main.a2351cc6.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/Neigbourhood_Map/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "a2351cc689155f902198",
+    "url": "/Neigbourhood_Map/static/css/main.fe2cf8c0.chunk.css"
+  },
+  {
+    "revision": "8287db1dc0652e96a56024ddca2e9ede",
+    "url": "/Neigbourhood_Map/index.html"
+  }
+];
